e6dac7bb53eeb66cb2b65f0b75034b8279b8a2a2
Rudra Pratap Singh <thakurrudra2602@gmail.com>
2026-06-14 11:15:23 +0530

    Fixed views syntax and secured API endpoints with IsAuthenticated

--- expenses/views.py
+++ expenses/views.py
@@ -4,6 +4,8 @@ from datetime import date
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes
 
 from .models import Expense
 from .forms import ExpenseForm
@@ -79,15 +81,39 @@ def delete_expense(request, pk):
 # 4. DJANGO REST FRAMEWORK API ENDPOINTS
 # ==========================================
 @api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
 def expense_api_list(request):
     if request.method == 'GET':
         expenses = Expense.objects.all()
         serializer = ExpenseSerializer(expenses, many=True)
         return Response(serializer.data)
-
     elif request.method == 'POST':
         serializer = ExpenseSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def expense_api_list_detail(request, pk):
+    try:
+        expense = Expense.objects.get(pk=pk)
+    except Expense.DoesNotExist:
+        return Response({'error': 'Expense not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ExpenseSerializer(expense)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ExpenseSerializer(expense, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        expense.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
